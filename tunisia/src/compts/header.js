0ad114129617afa77e509c3aa4a88045fce892e4
mohamed <76411950+mmoha-199@users.noreply.github.com>
2023-04-08 02:28:40 +0100

    start

---
+++ tunisia/src/compts/header.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+
+export default function Navbar(){
+   
+        return(
+            <nav>
+                <img className='logo'/>
+                <ul className='nav-list'>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>About</a></li>
+                    <li><a href='#'>Galery</a></li>
+                    <li><a href='#'>visit</a></li>
+                    <li><a href='#'>Shop</a></li>
+                </ul>
+            </nav>
+        )
+};
